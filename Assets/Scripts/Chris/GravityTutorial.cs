0e6d32520174e41ff3c75cae81af46ca648a5cc2
Steven Brown <stevenbrown_@hotmail.co.uk>
2014-02-10 14:43:35 +0000

    Level 1 and 6 tutorials are in.
    Both tutorials have been created to help the player understand the
    controls. Also textures have been added to the sides of the screen on
    mobile to remind players of the controls for movement.

---
+++ Assets/Scripts/Chris/GravityTutorial.cs
@@ -0,0 +1,74 @@
+﻿using UnityEngine;
+using System.Collections;
+
+//Christopher Stephens
+public class GravityTutorial : MonoBehaviour 
+{
+	public GUISkin guiskin;
+	public float buttonWidth;
+	public float buttonHeight;
+	
+	private Vector3 scale;
+	private float originalWidth = 1920;
+	private float originalHeight = 1080;
+	
+	private bool inTrigger = false;
+	private bool onMobile = false;
+	
+	void Start()
+	{
+		if(Application.platform == RuntimePlatform.Android || Application.platform == RuntimePlatform.OSXPlayer)
+		{
+			onMobile = true;
+		}
+	}
+	
+	void OnTriggerStay(Collider col)
+	{
+		if(col.transform.tag == "Player")
+		{
+			inTrigger = true;
+		}
+	}
+	
+	void OnTriggerExit(Collider col)
+	{
+		if(col.transform.tag == "Player")
+		{
+			inTrigger = false;
+		}
+	}
+	
+	void OnGUI()
+	{
+		GUI.skin = guiskin;
+			
+		scale.x = Screen.width/originalWidth;
+		scale.y = Screen.height/originalHeight;
+		scale.z = 1;
+		
+		// Save the original matrix
+		Matrix4x4 originalMatrix = GUI.matrix;
+		
+		GUI.matrix = Matrix4x4.TRS(Vector3.zero, Quaternion.identity, scale);
+		
+		if(onMobile == true)
+		{
+			if(inTrigger == false)
+			{
+				GUI.TextField(new Rect(originalWidth/2 - buttonWidth/2, originalHeight * 0.1f, buttonWidth, buttonHeight),
+					"Tapping the screen \nwill flip gravity!");	
+			}
+		}
+		else
+		{
+			if(inTrigger == false)
+			{
+				GUI.TextField(new Rect(originalWidth/2 - buttonWidth/2, originalHeight * 0.1f, buttonWidth, buttonHeight),
+					"Pressing space \nwill flip gravity!");
+			}
+		}
+		
+		GUI.matrix = originalMatrix;
+	}
+}
---
+++ Assets/Scripts/Ste/MovementTutorial.cs
@@ -0,0 +1,91 @@
+﻿using UnityEngine;
+using System.Collections;
+//Steven Brown 
+public class MovementTutorial : MonoBehaviour 
+{
+	public GUISkin guiskin;
+	public float buttonWidth;
+	public float buttonHeight;
+	
+	
+	private Vector3 scale;
+	private float originalWidth = 1920;
+	private float originalHeight = 1080;
+	
+	private bool moveRight, moveLeft = false;
+	private bool onMobile;
+	
+	void Start()
+	{
+		if(Application.platform == RuntimePlatform.Android || Application.platform == RuntimePlatform.OSXPlayer)
+		{
+			onMobile = true;
+		}	
+	}
+	void OnTriggerStay()
+	{
+		if(this.tag == "MoveRight")
+		{
+			moveRight = true;
+		}
+		else if(this.tag == "MoveLeft")
+		{
+			moveLeft = true;	
+		}
+	}
+	
+	void OnTriggerExit()
+	{
+		if(this.tag == "MoveRight")
+		{
+			moveRight = false;
+		}
+		else if(this.tag == "MoveLeft")
+		{
+			moveLeft = false;	
+		}
+	}
+	
+	void OnGUI()
+	{
+
+		GUI.skin = guiskin;
+			
+		scale.x = Screen.width/originalWidth;
+		scale.y = Screen.height/originalHeight;
+		scale.z = 1;
+		
+		// Save the original matrix
+		Matrix4x4 originalMatrix = GUI.matrix;
+		
+		GUI.matrix = Matrix4x4.TRS(Vector3.zero, Quaternion.identity, scale);
+		if(onMobile)
+		{
+			if(moveLeft)
+			{
+				GUI.Label(new Rect( originalWidth/2 - buttonWidth/2, originalHeight * 0.01f, buttonWidth, buttonHeight), 
+					"Touch < to move left");
+			}
+			else if(moveRight)
+			{
+				GUI.Label(new Rect( originalWidth/2 - buttonWidth/2, originalHeight * 0.01f, buttonWidth, buttonHeight), 
+					"Touch > to move right");
+			}
+		}
+		else 
+		{
+			if(moveLeft)
+			{
+				GUI.Label(new Rect( originalWidth/2 - buttonWidth/2, originalHeight * 0.01f, buttonWidth, buttonHeight), 
+					"Press 'a' to move left");
+			}
+			else if(moveRight)
+			{
+				GUI.Label(new Rect( originalWidth/2 - buttonWidth/2, originalHeight * 0.01f, buttonWidth, buttonHeight), 
+					"Press 'd' to move right");
+			}
+		}
+		GUI.matrix = originalMatrix;
+	}
+	
+}
